dbe8213d1fa25d78e8908dbb01bb5166da205f47
Tayfun Açıcı <tyfnacici@gmail.com>
2023-06-22 13:51:51 +0300

    feat(cards): add cards section

--- getir-clone-tailwind/src/components/Card.js
+++ getir-clone-tailwind/src/components/Card.js
@@ -1,14 +1,14 @@
 import React from "react"
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="flex flex-col items-center bg-white shadow-lg p-5 rounded-xl">
-      <div className="w-36 h-36 rounded-full bg-red-400"></div>
+      <img className="w-36 h-36" alt={props.title} src={props.img}/>
       <div className="flex flex-col text-center space-y-4 w-[22rem]">
         <h1 className="text-purple-800 font-bold text-xl pt-4">
-          A promotion for every order
+          {props.title}
         </h1>
-        <p className="text-gray-500">At Getir, you can find a promotion for every order.</p>
+        <p className="text-gray-500">{props.desc}</p>
       </div>
     </div>
   )
