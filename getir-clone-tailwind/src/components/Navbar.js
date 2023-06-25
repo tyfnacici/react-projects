1a5cafe65886698927670fc1a38b3634e86c1420
Tayfun Açıcı <tyfnacici@gmail.com>
2023-06-25 15:15:34 +0300

    fix(navbar): fix some styles on navbar

--- getir-clone-tailwind/src/components/Navbar.js
+++ getir-clone-tailwind/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react"
+import Reactz from "react"
 import getir from "../images/getir.svg"
 import getiryemek from "../images/getir-yemek.svg"
 import getirbüyük from "../images/getir-büyük.png"
@@ -7,18 +7,20 @@ import getircarsi from "../images/getir-carsi.png"
 
 const Navbar = () => {
   return (
-    <div className="p-3 bg-[#5e3fbc] text-white text-base font-semibold">
-      <div className="md:container mx-auto px-44 flex justify-between">
-        <div className="flex items-center">
-          <ul className="flex space-x-9 mt-2">
-            <img src={getir} alt="getir" className="h-4" />
-            <img src={getiryemek} alt="getiryemek" className="h-4" />
-            <img src={getirbüyük} alt="getirbüyük" className="h-4" />
-            <img src={getirsu} alt="getirsu" className="h-4" />
-            <img src={getircarsi} alt="getircarsi" className="h-4" />
+    <div className="pt-[.20rem] bg-[#4d3399] text-white text-base font-semibold">
+      <div className="md:container mx-auto px-36 flex justify-between ">
+        <div className="flex items-center space-x-[1.25rem]">
+          <div className="relative bg-[#5d3ebc] px-5 rounded-t-lg">
+            <img src={getir} alt="getir" className="h-4 static mb-3 mt-3" />
+          </div>
+          <ul className="flex space-x-[2rem]">
+            <img src={getiryemek} alt="getiryemek" className="h-4 mt-3 mb-3" />
+            <img src={getirbüyük} alt="getirbüyük" className=" h-4 mt-3 mb-3" />
+            <img src={getirsu} alt="getirsu" className="h-4 mt-3 mb-3" />
+            <img src={getircarsi} alt="getircarsi" className="h-4 mt-3 mb-3" />
           </ul>
         </div>
-        <div className="flex items-end">
+        <div className="flex items-end pt-3 pb-3">
           <ul className="flex space-x-8 text-sm">
             <li className="flex items-center space-x-1.5">
               <i className="pi pi-globe text-lg"></i>
